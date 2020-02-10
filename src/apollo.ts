aac4b4776e3ab924c1a2ac185b03ed1d4f7f1833
Dongin Kim <miknignod@gmail.com>
2020-02-10 11:01:23 +0900

    Apollo setup

---
+++ src/apollo.ts
@@ -0,0 +1,50 @@
+import ApolloClient, { PresetConfig, Operation } from 'apollo-boost';
+
+const presetConfig: PresetConfig = {
+  request: (operation: Operation) => {
+    operation.setContext({
+      headers: {
+        'X-JWT': localStorage.getItem('jwt') || '',
+      },
+    });
+  },
+  uri: 'http://localhost:4000/graphql',
+  clientState: {
+    defaults: {
+      auth: {
+        __typename: 'Auth',
+        isLoggedIn: Boolean(localStorage.getItem('jwt')),
+      },
+    },
+    resolvers: {
+      Mutation: {
+        logUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem('jwt', token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: 'Auth',
+                isLoggedIn: true,
+              },
+            },
+          });
+          return null;
+        },
+        logUserOut: (_, __, { cache }) => {
+          localStorage.removeItem('jwt');
+          cache.writeData({
+            data: {
+              __typename: 'Auth',
+              isLoggedIn: false,
+            },
+          });
+          return null;
+        },
+      },
+    },
+  },
+};
+
+const client = new ApolloClient(presetConfig);
+
+export default client;
