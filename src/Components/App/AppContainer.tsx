b3cab92fc82d4344041ae9eb27ba3023b0f7c9dc
Dongin Kim <miknignod@gmail.com>
2020-02-10 14:00:36 +0900

    Typescript and Styled Components

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
-
 import { IS_LOGGED_IN } from './AppQueries';
 import AppPresenter from './AppPresenter';
+import { ThemeProvider } from '../../typed-components';
+import theme from '../../theme';
 
 const AppContainer = ({ data }: { data?: any }) => {
-  return <AppPresenter isLoggedIn={data.auth.isLoggedIn} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    </ThemeProvider>
+  );
 };
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
