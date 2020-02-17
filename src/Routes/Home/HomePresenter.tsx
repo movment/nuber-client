e8cd9ae95afa0234af40626a6851d86d6eb72b84
Dongin Kim <miknignod@gmail.com>
2020-02-17 17:21:46 +0900

    Creating Route Markers

--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -1,43 +1,96 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Sidebar from 'react-sidebar';
-import styled from '../../typed-components';
+import AddressBar from '../../Components/AddressBar';
+import Button from '../../Components/Button';
 import Menu from '../../Components/Menu';
+import styled from '../../typed-components';
 
 const Container = styled.div``;
+
+const MenuButton = styled.button`
+  appearance: none;
+  padding: 10px;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  text-align: center;
+  font-weight: 800;
+  border: 0;
+  cursor: pointer;
+  font-size: 20px;
+  transform: rotate(90deg);
+  z-index: 2;
+  background-color: transparent;
+`;
+
+const Map = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`;
+
+const ExtendedButton = styled(Button)`
+  position: absolute;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  z-index: 10;
+  height: auto;
+  width: 80%;
+`;
 
 interface IProps {
   isMenuOpen: boolean;
   toggleMenu: () => void;
   loading: boolean;
+  mapRef: any;
+  toAddress: string;
+  onAddressSubmit: () => void;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const HomePresenter: React.SFC<IProps> = ({
   isMenuOpen,
   toggleMenu,
   loading,
-}) => {
-  return (
-    <Container>
-      <Helmet>
-        <title>Home | Nuber</title>
-      </Helmet>
-      <Sidebar
-        sidebar={<Menu />}
-        open={isMenuOpen}
-        onSetOpen={toggleMenu}
-        styles={{
-          sidebar: {
-            width: '80%',
-            background: 'white',
-            zIndex: '10',
-          },
-        }}
-      >
-        {!loading && <button onClick={toggleMenu}>Open Sidebar</button>}
-      </Sidebar>
-    </Container>
-  );
-};
+  toAddress,
+  mapRef,
+  onInputChange,
+  onAddressSubmit,
+}) => (
+  <Container>
+    <Helmet>
+      <title>Home | Number</title>
+    </Helmet>
+    <Sidebar
+      sidebar={<Menu />}
+      open={isMenuOpen}
+      onSetOpen={toggleMenu}
+      styles={{
+        sidebar: {
+          backgroundColor: 'white',
+          width: '80%',
+          zIndex: '10',
+        },
+      }}
+    >
+      {!loading && <MenuButton onClick={toggleMenu}>|||</MenuButton>}
+      <AddressBar
+        name={'toAddress'}
+        onChange={onInputChange}
+        value={toAddress}
+        onBlur={null}
+      />
+      <ExtendedButton
+        onClick={onAddressSubmit}
+        disabled={toAddress === ''}
+        value={'Pick Address'}
+      />
+      <Map ref={mapRef} />
+    </Sidebar>
+  </Container>
+);
 
 export default HomePresenter;
